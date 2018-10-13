7b24293eb4d2a25a763e4f131007836bf3dbfd8e
Aliaksei Piatrouski <apiatrouski@expedia.com>
2018-10-13 15:41:48 -0700

    asd

--- src/test/java/uiAutomation/tests/search/GoogleSearchTest.java
+++ src/test/java/uiAutomation/tests/search/GoogleSearchTest.java
@@ -46,42 +46,4 @@ public class GoogleSearchTest extends BaseTest {
 
         Assert.assertEquals(actualPageTitle, expectedPageTitle);
     }
-
-    @Test
-    public void test003() {
-        System.out.println("3");
-    }
-
-    @Test
-    public void test004() {
-        System.out.println("4");
-    }
-
-    @Test
-    public void test005() {
-        System.out.println("5");
-    }
-
-    @Test
-    public void test006() {
-        System.out.println("6");
-    }
-
-    @Test
-    public void test007() {
-        System.out.println("7");
-    }
-
-    @Test
-    public void test008() {
-        System.out.println("8");
-    }
-
-    @Test
-    public void test009() {
-        System.out.println("9");
-    }
-
-
-
 }
